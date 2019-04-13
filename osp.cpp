5f8d63157ad12bf50be77393d21f7f4af97649f3
shubham <41983918+starboysk@users.noreply.github.com>
2019-04-13 22:33:43 +0530

    3rd commit

--- osp.cpp
+++ osp.cpp
@@ -2,24 +2,24 @@
 using namespace std;
 const int np = 5;
 const int nr = 4;
-bool finish[np];
+static bool finish[np];
 int i;
 int j;
 int count=0;
-int avail[nr] = {1,5,2,0};
-int work[nr];
-int  allo[np][nr] = {{0,0,1,2},
+static int avail[nr] = {1,5,2,0};
+static int work[nr];
+static int  allo[np][nr] = {{0,0,1,2},
  					{1,0,0,0},
 					{1,3,5,4},
 					{0,6,3,2},
 					{0,0,1,4}};
-int  mx[np][nr] = {{0,0,1,2},
+static int  mx[np][nr] = {{0,0,1,2},
 				   {1,7,5,0},
 				   {2,3,5,6},
 				   {0,6,5,2},
 				   {0,6,5,6}};
 
-int need[np][nr];
+static int need[np][nr];
 
 void calneed(int all[np][nr],int mm[np][nr]){
 	for(i = 0; i<np; i++){
@@ -27,8 +27,12 @@ void calneed(int all[np][nr],int mm[np][nr]){
 			need[i][j]=mm[i][j]-all[i][j];
 		}
 	}
+//		for(j=0;j<nr;j++){
+//	        printf("\n %d",avail[i]);
+//			}
 }
 bool check(int a,int temp[nr]){
+	int count;
 	for(int k=0;k<nr;k++){
 		
 		if(need[a][k]<=temp[k]){
@@ -47,37 +51,26 @@ bool check(int a,int temp[nr]){
 	
 	for(j=0;j<nr;j++){
 		work[j]=allo[t][j]+work[j];	
-		printf("\n %d",avail[t]);
+		
 	}
 	for(j=0;j<nr;j++){
-	        printf("\n %d",avail[i]);
+	        printf("\n %d",work[j]);
 			}
-}
-//void initavail(){
-//	for(j=0;j<nr;j++){
-//		int gett;
-//		scanf("%d",&gett);
-//		avail[j]=gett;	
-//		
-//	}
-//}
+	}
+	
+
 int main(){
-	  work[np]=avail[np];// Step 1
-	//initavail();
+	 								// Step 1
 	for(i=0;i<nr;i++){
 		work[i] = avail[i];
 	}
 	for(j=0;j<nr;j++){
-	        printf("\n %d",work[i]);
+	        printf("\n %d",work[j]);
 	        
 			}
 	for(i=0;i<np;i++){
 		finish[i] = false;
 	}              //     step 1 finished
-//	for(i=0;i<np;i++){
-//		printf("%d",finish[i]);
-//		printf("%s", finish[i]?"true":"false");
-//	}
 	calneed(allo,mx);
 	for(i=0;i<np;i++){
 		printf("\n");
